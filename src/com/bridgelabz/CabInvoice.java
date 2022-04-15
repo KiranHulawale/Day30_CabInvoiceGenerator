9be9c6acbdbe898fba0ecc714c0496c395061190
Kiran <kiranhulawale123@gmail.com>
2022-04-15 19:45:09 +0530

    Added UC-2 to calculate fare for multiple rides

--- src/com/bridgelabz/CabInvoice.java
+++ src/com/bridgelabz/CabInvoice.java
@@ -2,10 +2,14 @@ package com.bridgelabz;
 
 public class CabInvoice {
 
-    static InvoiceGenerator invoice = new InvoiceGenerator();
 
     public static void main(String[] args) {
-
-        System.out.println(invoice.generateInvoice(3,3));
+        InvoiceGenerator invoice = new InvoiceGenerator();
+        System.out.println(invoice.generateInvoice(7.5, 4));
+        Ride[] rides = {new Ride(6, 3),
+                new Ride(5,3),
+                new Ride(9,6)
+        };
+        System.out.println(invoice.multiplefare(rides));
     }
-}
+    }
--- src/com/bridgelabz/InvoiceGenerator.java
+++ src/com/bridgelabz/InvoiceGenerator.java
@@ -1,23 +1,23 @@
 package com.bridgelabz;
 
 public class InvoiceGenerator {
-
-    public double distance;
-    public double time;
-
-    public InvoiceGenerator() {
-        super();
-        this.distance = distance;
-        this.time = time;
-    }
+    
 
     int costPerKm = 10;
     int costPerMin = 1;
-    //int minFare = 5;
+
     public double generateInvoice(double distance, int time) {
-
         double fare =  distance * costPerKm + time * costPerMin;
-
         return fare > 5 ? fare : 5;
+    }
+    
+    public double multiplefare(Ride[] rides) {
+        double singleFare;
+        double totalFare = 0;
+        for (Ride ride  : rides) {
+            singleFare = totalFare + generateInvoice(ride.distance, (int) ride.time);
+            totalFare += singleFare > 5 ? singleFare : 5;
+        }
+        return totalFare;
     }
 }
